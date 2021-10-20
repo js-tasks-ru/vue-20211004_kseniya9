2d4c712cc3856dd0e2c9722d0072f10bf321fe85
Kseniya Fedoseeva <fedoseeva.datsenko@gmail.com>
2021-10-20 20:38:47 +0300

    задача 2-3

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -29,4 +29,32 @@ const emails = [
   'Isaias_Kuhic@jarrett.net',
 ];
 
-// Требуется создать Vue приложение
+const MarkedList = {
+  data() {
+    return {
+      items: emails,
+      searchValue: undefined,
+    };
+  },
+  computed: {
+    filterItems() {
+      let listEmail = [];
+
+      for (let i = 0; i < this.items.length; i += 1) {
+        let isActive = () => {
+          if (this.items[i].includes(this.searchValue)) {
+            return true;
+          } else {
+            return false;
+          }
+        };
+        listEmail[i] = {
+          item: this.items[i],
+          isActive: isActive(),
+        };
+      }
+      return listEmail;
+    },
+  },
+};
+createApp(MarkedList).mount('#app');
